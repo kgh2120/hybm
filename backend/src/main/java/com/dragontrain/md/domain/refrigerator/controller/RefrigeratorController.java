c11492c4acdbd265950a5838870220c908e2f08e
nangmandev <nangmandeveloper@gmail.com>
2024-04-30 17:38:36 +0900

    feat: 냉장고 컨트롤러 작성입니다.

---
+++ backend/src/main/java/com/dragontrain/md/domain/refrigerator/controller/RefrigeratorController.java
@@ -0,0 +1,27 @@
+package com.dragontrain.md.domain.refrigerator.controller;
+
+import com.dragontrain.md.domain.refrigerator.controller.Response.StorageDesignsResponse;
+import com.dragontrain.md.domain.refrigerator.service.StorageDesignService;
+import com.dragontrain.md.domain.refrigerator.service.StorageStorageDesignService;
+import com.dragontrain.md.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/refrigerators")
+public class RefrigeratorController {
+
+	private final StorageStorageDesignService storageStorageDesignService;
+
+	@GetMapping("/designs")
+	public StorageDesignsResponse findAllRefrigeratorDesigns(
+		@AuthenticationPrincipal User user
+		) {
+		return storageStorageDesignService.findAllStorageDesign(user);
+	}
+
+}
