575b583c0cffd368c85cbf9b78026dd071837669
nangmandev <nangmandeveloper@gmail.com>
2024-04-30 17:38:25 +0900

    feat: 냉장고 전체 디자인 요청시 반환 DTO작성입니다.

---
+++ backend/src/main/java/com/dragontrain/md/domain/refrigerator/controller/Response/StorageDesignResponse.java
@@ -0,0 +1,18 @@
+package com.dragontrain.md.domain.refrigerator.controller.Response;
+
+import com.dragontrain.md.domain.refrigerator.domain.StorageTypeId;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class StorageDesignResponse {
+	private Integer storageDesignId;
+	private String name;
+	private String designImgSrc;
+	private Boolean isApplied;
+	private Integer level;
+	private Boolean has;
+	private StorageTypeId location;
+}
---
+++ backend/src/main/java/com/dragontrain/md/domain/refrigerator/controller/Response/StorageDesignsResponse.java
@@ -0,0 +1,29 @@
+package com.dragontrain.md.domain.refrigerator.controller.Response;
+
+import com.dragontrain.md.domain.refrigerator.domain.StorageTypeId;
+import lombok.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class StorageDesignsResponse {
+	private List<StorageDesignResponse> cool;
+	private List<StorageDesignResponse> ice;
+	private List<StorageDesignResponse> cabinet;
+
+	public static StorageDesignsResponse createByStorageType(List<StorageDesignResponse> designs) {
+		Map<StorageTypeId, List<StorageDesignResponse>> res = designs.stream()
+			.collect(Collectors.groupingBy(StorageDesignResponse::getLocation));
+
+		return StorageDesignsResponse.builder()
+			.cool(res.get(StorageTypeId.COOL))
+			.ice(res.get(StorageTypeId.ICE))
+			.cabinet(res.get(StorageTypeId.CABINET))
+			.build();
+	}
+}
