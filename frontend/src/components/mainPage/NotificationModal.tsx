fb807a090ee3f3bcba30c31cdae189b2990c43e4
wkd940709 <wkd940709@naver.com>
2024-04-30 09:59:06 +0900

    feat: 알림모달 알림함비우기 버튼 추가

--- frontend/src/components/mainPage/NotificationModal.tsx
+++ frontend/src/components/mainPage/NotificationModal.tsx
@@ -4,6 +4,10 @@ import NotificationItem from "./NotificationItem";
 function NotificationModal() {
   return (
     <div className={styles.notification_modal_wrapper}>
+      <button className={styles.remove_all_button}>알림함 비우기</button>
+      <NotificationItem />
+      <NotificationItem />
+      <NotificationItem />
       <NotificationItem />
       <NotificationItem />
     </div>
