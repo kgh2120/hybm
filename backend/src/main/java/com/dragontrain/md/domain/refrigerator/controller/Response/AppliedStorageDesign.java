5a4a0679f8135e58efb80ff3c9bd7d0a9039e57c
nangmandev <nangmandeveloper@gmail.com>
2024-05-01 01:46:39 +0900

    feat: 사용중인 냉장고 디자인 DTO, 반환 DTO 작성

---
+++ backend/src/main/java/com/dragontrain/md/domain/refrigerator/controller/Response/AppliedStorageDesign.java
@@ -0,0 +1,14 @@
+package com.dragontrain.md.domain.refrigerator.controller.Response;
+
+import com.dragontrain.md.domain.refrigerator.domain.StorageTypeId;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class AppliedStorageDesign {
+	Integer id;
+	String imgSrc;
+	StorageTypeId type;
+}
