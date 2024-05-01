8017a8819e0912b2b57d2e389a78fb415414701c
김규현 <kgh2120@gmail.com>
2024-05-01 14:22:23 +0900

    feat: 소셜 로그인 시 쿠키 반환

---
+++ backend/src/main/java/com/dragontrain/md/common/config/utils/CookieUtils.java
@@ -0,0 +1,30 @@
+package com.dragontrain.md.common.config.utils;
+
+import jakarta.servlet.http.Cookie;
+
+public class CookieUtils {
+
+	private static final String COOKIE_KEY_REFRESH_TOKEN = "refresh_token";
+	private static final String COOKIE_KEY_ACCESS_TOKEN = "access_token";
+	private static final String COOKIE_ATTRIBUTE_NAME_SAME_SITE = "same-site";
+	private static final String COOKIE_SAME_SITE_NONE = "none";
+	private static final String COOKIE_REFRESH_TOKEN_PATH = "/api/refresh";
+	private static final String COOKIE_ACCESS_TOKEN_PATH = "/";
+
+	public static Cookie makeCookie(String key, String value, String path, int maxAge){
+		Cookie cookie = new Cookie(key, value);
+		cookie.setAttribute(COOKIE_ATTRIBUTE_NAME_SAME_SITE, COOKIE_SAME_SITE_NONE);
+		cookie.setPath(path);
+		cookie.setHttpOnly(true);
+		cookie.setSecure(true);
+		cookie.setMaxAge(maxAge);
+		return cookie;
+	}
+
+	public static Cookie makeRefreshTokenCookie(String value, int maxAge){
+		return makeCookie(COOKIE_KEY_REFRESH_TOKEN, value, COOKIE_REFRESH_TOKEN_PATH, maxAge);
+	}
+	public static Cookie makeAccessTokenCookie(String value, int maxAge){
+		return makeCookie(COOKIE_KEY_ACCESS_TOKEN, value, COOKIE_ACCESS_TOKEN_PATH, maxAge);
+	}
+}
--- backend/src/main/java/com/dragontrain/md/domain/user/oauth/OAuth2SuccessHandler.java
+++ backend/src/main/java/com/dragontrain/md/domain/user/oauth/OAuth2SuccessHandler.java
@@ -6,6 +6,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import com.dragontrain.md.common.config.jwt.JwtProvider;
+import com.dragontrain.md.common.config.jwt.Token;
+import com.dragontrain.md.common.config.utils.CookieUtils;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -18,6 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
 	private final CustomAuthorizationRequestRepository customAuthorizationRequestRepository;
+	private final JwtProvider jwtProvider;
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -25,9 +30,19 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 		AbstractCustomOAuth2Client oAuth2Client = (AbstractCustomOAuth2Client)authentication.getPrincipal();
 		log.debug("userId : {} is entered. SocialType : {}, Email : {}", oAuth2Client.getUserId(), oAuth2Client.getSocialLoginType().name(),
 			oAuth2Client.getUserEmail());
-		// TODO JWT 설정
+
+		Long userId = oAuth2Client.getUserId();
+		createAndRegisterTokens(response, userId);
+
 		String redirectUri = customAuthorizationRequestRepository.getRedirectUri(request);
 		customAuthorizationRequestRepository.removeCustomOAuth2AuthorizationRequest(request);
 		getRedirectStrategy().sendRedirect(request, response, redirectUri);
+	}
+
+	private void createAndRegisterTokens(HttpServletResponse response, Long userId) {
+		Token accessToken = jwtProvider.createAccessToken(userId);
+		Token refreshToken = jwtProvider.createRefreshToken(userId);
+		response.addCookie(CookieUtils.makeAccessTokenCookie(accessToken.getValue(), accessToken.getTtlToSecond()));
+		response.addCookie(CookieUtils.makeRefreshTokenCookie(refreshToken.getValue(), refreshToken.getTtlToSecond()));
 	}
 }
