0f894e40067b0b8bbdf8e93463388930ab3a6a50
nangmandev <nangmandeveloper@gmail.com>
2024-05-01 20:58:01 +0900

    feat: jpa repo 이름 교체

--- backend/src/main/java/com/dragontrain/md/domain/refrigerator/infra/JpaRefrigeratorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dragontrain.md.domain.refrigerator.infra;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dragontrain.md.domain.refrigerator.domain.Refrigerator;
-
-public interface JpaRefrigeratorRepository extends JpaRepository<Refrigerator, Long> {
-}
