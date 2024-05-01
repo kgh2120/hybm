a685b39915485fef7f8b962656f5831c95454f80
nangmandev <nangmandeveloper@gmail.com>
2024-05-01 20:58:21 +0900

    feat: jpa repo 통폐합

--- backend/src/main/java/com/dragontrain/md/domain/refrigerator/infra/StorageStorageDesignJpaRepository.java
+++ backend/src/main/java/com/dragontrain/md/domain/refrigerator/infra/StorageStorageDesignJpaRepository.java
@@ -1,11 +1,14 @@
 package com.dragontrain.md.domain.refrigerator.infra;
 
 import com.dragontrain.md.domain.refrigerator.controller.Response.StorageDesignResponse;
+import com.dragontrain.md.domain.refrigerator.domain.StorageDesign;
 import com.dragontrain.md.domain.refrigerator.domain.StorageStorageDesign;
+import com.dragontrain.md.domain.refrigerator.domain.StorageTypeId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StorageStorageDesignJpaRepository extends JpaRepository<StorageStorageDesign, Long> {
 	@Query("select new com.dragontrain.md.domain.refrigerator.controller.Response.StorageDesignResponse" +
