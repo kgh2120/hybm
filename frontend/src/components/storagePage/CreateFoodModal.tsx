ff04e1e4989e093e3cac845ae0d90d1cc24b5e54
JangHyunsu <wkd940709@naver.com>
2024-05-01 18:56:51 +0900

    feat: 버튼 컴포넌트, 확인모달 컴포넌트 생성

---
+++ frontend/src/components/storagePage/CreateFoodModal.tsx
@@ -0,0 +1,9 @@
+import Button from '../common/Button'
+
+function CreateFoodModal() {
+  return (
+    <div><Button content='완료' color='red'/></div>
+  )
+}
+
+export default CreateFoodModal
--- frontend/src/pages/StoragePage.tsx
+++ frontend/src/pages/StoragePage.tsx
@@ -1,6 +1,26 @@
+import { useState } from 'react';
+import Modal from '../components/common/Modal';
+import CreateFoodModal from '../components/storagePage/CreateFoodModal';
+import styles from '../styles/storagePage/StoragePage.module.css'
+
 function StoragePage() {
+  const [isCreateFoodModalOpen, setIsCreateFoodModalOpen] = useState(false);
+  const handleOpenCreateFoodModal = () => {
+    setIsCreateFoodModalOpen(true);
+  };
+
+  const handleCloseCreateFoodModal = () => {
+    setIsCreateFoodModalOpen(false);
+  };
   return (
-    <div>StoragePage</div>
+    <div className={styles.wrapper}>
+      <button onClick={handleOpenCreateFoodModal}>식품등록모달</button>
+      {isCreateFoodModalOpen && (
+        <Modal title="식품 등록" clickEvent={handleCloseCreateFoodModal}>
+          <CreateFoodModal />
+        </Modal>
+      )}
+    </div>
   )
 }
 
