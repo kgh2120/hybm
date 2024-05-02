5059275060e260e06332afec42ae268d96fcd230
김규현 <kgh2120@gmail.com>
2024-05-03 08:57:03 +0900

    feat: 로그아웃 기능 개발
    로그아웃 API를 보내면 엑세스 토큰, 리프래시 토큰 쿠키를 만료시킵니다.

--- backend/src/main/java/com/dragontrain/md/domain/user/controller/UserController.java
+++ backend/src/main/java/com/dragontrain/md/domain/user/controller/UserController.java
@@ -2,27 +2,36 @@ package com.dragontrain.md.domain.user.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dragontrain.md.common.config.exception.ErrorResponse;
 import com.dragontrain.md.common.config.exception.GlobalErrorCode;
+import com.dragontrain.md.common.config.utils.CookieUtils;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RequestMapping("/api/users")
 @RestController
 public class UserController {
 
 	@GetMapping("/is-login")
-	public ResponseEntity<Void> isLogin(){
+	public ResponseEntity<Void> isLogin() {
 		return ResponseEntity.ok().build();
 	}
+
 	@GetMapping("/login-fail")
-	public ResponseEntity<ErrorResponse> failLogin(HttpServletRequest request){
+	public ResponseEntity<ErrorResponse> failLogin(HttpServletRequest request) {
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
 			.body(ErrorResponse.createErrorResponse(GlobalErrorCode.UNAUTHORIZED_ACCESS, "/"));
+	}
+
+	@GetMapping("/logout")
+	public ResponseEntity<Void> logout(HttpServletResponse response) {
+		response.addCookie(CookieUtils.deleteAccessTokenCookie());
+		response.addCookie(CookieUtils.deleteRefreshTokenCookie());
+		return ResponseEntity.ok().build();
 	}
 }
