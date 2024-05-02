a4490d843cc83e189fe7669c9a0eafc0d14e8c79
nangmandev <nangmandeveloper@gmail.com>
2024-05-02 15:12:24 +0900

    feat: 컨트롤러 작성 완료

--- backend/src/main/java/com/dragontrain/md/domain/statistics/controller/StatisticsController.java
+++ backend/src/main/java/com/dragontrain/md/domain/statistics/controller/StatisticsController.java
@@ -1,9 +1,14 @@
 package com.dragontrain.md.domain.statistics.controller;
 
+import com.dragontrain.md.domain.statistics.controller.response.StatisticsResponse;
+import com.dragontrain.md.domain.statistics.service.StatisticsService;
+import com.dragontrain.md.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -11,5 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/statistics")
 public class StatisticsController {
 
+	private final StatisticsService statisticsService;
 
+	@GetMapping("/month")
+	public ResponseEntity<StatisticsResponse> findStatisticsByYearAndMonth(
+		@AuthenticationPrincipal User user,
+		@RequestParam Integer year,
+		@RequestParam Integer month
+	){
+		return ResponseEntity.ok(statisticsService.findStatisticsByYearAndMonth(user, year, month));
+	}
 }
