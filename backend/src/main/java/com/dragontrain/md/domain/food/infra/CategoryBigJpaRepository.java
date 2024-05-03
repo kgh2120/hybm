b64ceb2313f84cde56e86dc6ff9a1e2730c1095e
nangmandev <nangmandeveloper@gmail.com>
2024-05-03 10:59:46 +0900

    feat: 대분류별 가격 조회

--- backend/src/main/java/com/dragontrain/md/domain/food/infra/CategoryBigJpaRepository.java
+++ backend/src/main/java/com/dragontrain/md/domain/food/infra/CategoryBigJpaRepository.java
@@ -1,8 +1,19 @@
 package com.dragontrain.md.domain.food.infra;
 
 import com.dragontrain.md.domain.food.domain.CategoryBig;
+import com.dragontrain.md.domain.statistics.service.dto.BigCategoryStatistics;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface CategoryBigJpaRepository extends JpaRepository<CategoryBig, Integer> {
+	@Query("select new com.dragontrain.md.domain.statistics.service.dto.BigCategoryStatistics" +
+		"(cb.name, f.price)" +
+		" from Food f join f.categoryDetail cd join cd.categoryBig cb" +
+		" on f.refrigerator.refrigeratorId=:refrigeratorId and f.categoryDetail=cd and cd.categoryBig=cb" +
+		" where year(f.createdAt)=:year and month(f.createdAt)=:month")
+	List<BigCategoryStatistics> findAllBigGroupAndSpend(Long refrigeratorId, Integer year, Integer month);
 
 }
