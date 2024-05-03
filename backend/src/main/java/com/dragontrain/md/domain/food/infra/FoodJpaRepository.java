78123dc057e750966343871fcf1ffbccb26e3a02
nangmandev <nangmandeveloper@gmail.com>
2024-05-03 15:41:44 +0900

    feat: 현재 연월에 해당하는 먹거나 버려서 삭제한 음식 조회

--- backend/src/main/java/com/dragontrain/md/domain/food/infra/FoodJpaRepository.java
+++ backend/src/main/java/com/dragontrain/md/domain/food/infra/FoodJpaRepository.java
@@ -4,6 +4,7 @@ import com.dragontrain.md.domain.refrigerator.domain.StorageTypeId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dragontrain.md.domain.food.domain.Food;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +12,9 @@ import java.util.Optional;
 public interface FoodJpaRepository extends JpaRepository<Food, Long> {
 
 	List<Food> findAllByRefrigerator_RefrigeratorIdAndStorageType_StorageType(Long refrigeratorId, StorageTypeId storageType);
+
+	@Query("select f from Food f join f.refrigerator r" +
+		" where r.refrigeratorId=:refrigeratorId and (f.foodDeleteType='EATEN' or f.foodDeleteType='THROWN')" +
+		" and year(f.deletedAt)=:year and month(f.deletedAt)=:month")
+	List<Food> findAllDeletedFoodByRefrigeratorIdAndTime(Long refrigeratorId, Integer year, Integer month);
 }
