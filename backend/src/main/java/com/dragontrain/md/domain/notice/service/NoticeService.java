2050f1dc8752a287f5034c0b1cd822e1c4245ba2
nangmandev <nangmandeveloper@gmail.com>
2024-05-05 16:41:15 +0900

    feat: 자신의 냉장고 조회 후 해당 식품들에 대한 알림을 조회

--- backend/src/main/java/com/dragontrain/md/domain/notice/service/NoticeService.java
+++ backend/src/main/java/com/dragontrain/md/domain/notice/service/NoticeService.java
@@ -1,8 +1,9 @@
 package com.dragontrain.md.domain.notice.service;
 
 import com.dragontrain.md.domain.notice.controller.response.AllNoticeResponse;
+import com.dragontrain.md.domain.user.domain.User;
 import org.springframework.data.domain.Pageable;
 
 public interface NoticeService {
-	AllNoticeResponse findAllNotDeletedNotice(Pageable pageable);
+	AllNoticeResponse findAllNotDeletedNotice(User user, Pageable pageable);
 }
