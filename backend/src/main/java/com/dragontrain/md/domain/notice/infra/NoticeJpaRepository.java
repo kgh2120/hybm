29f7503ee3635e8f6e489c3f9cc758fa2125478e
nangmandev <nangmandeveloper@gmail.com>
2024-05-05 16:39:16 +0900

    feat: 자신의 식품에 대한 알림만 조회할 수 있도록 수정

--- backend/src/main/java/com/dragontrain/md/domain/notice/infra/NoticeJpaRepository.java
+++ backend/src/main/java/com/dragontrain/md/domain/notice/infra/NoticeJpaRepository.java
@@ -4,7 +4,10 @@ import com.dragontrain.md.domain.notice.domain.Notice;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NoticeJpaRepository extends JpaRepository<Notice, Long> {
-	Slice<Notice> findAllByDeletedAtIsNull(Pageable pageable);
+	@Query("select n from Notice n join n.food f join f.refrigerator r" +
+		   " on r.refrigeratorId=:refrigeratorId and n.deletedAt is null")
+	Slice<Notice> findAllNotDeletedNotice(Long refrigeratorId, Pageable pageable);
 }
