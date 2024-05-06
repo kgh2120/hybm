dfa4695b30098063155be370884d70cf1e37f92a
nangmandev <nangmandeveloper@gmail.com>
2024-05-06 20:56:08 +0900

    feat: 내 냉장고의 삭제되지 않은 미확인알림여부 반환 메서드 작성

--- backend/src/main/java/com/dragontrain/md/domain/notice/infra/NoticeJpaRepository.java
+++ backend/src/main/java/com/dragontrain/md/domain/notice/infra/NoticeJpaRepository.java
@@ -10,4 +10,6 @@ public interface NoticeJpaRepository extends JpaRepository<Notice, Long> {
 	@Query("select n from Notice n join n.food f join f.refrigerator r" +
 		   " on r.refrigeratorId=:refrigeratorId and n.deletedAt is null")
 	Slice<Notice> findAllNotDeletedNotice(Long refrigeratorId, Pageable pageable);
+
+	Boolean existsByFood_Refrigerator_RefrigeratorIdAndDeletedAtIsNullAndIsCheckedIsFalse(Long refrigeratorId);
 }
