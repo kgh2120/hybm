3d02faca2dd6f244c4b08f8bb2bdcf71964720bf
nangmandev <nangmandeveloper@gmail.com>
2024-05-07 12:33:35 +0900

    feat: 이미 삭제된 경우, 삭제불가인 경우 에러코드 추가

--- backend/src/main/java/com/dragontrain/md/domain/notice/exception/NoticeErrorCode.java
+++ backend/src/main/java/com/dragontrain/md/domain/notice/exception/NoticeErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum NoticeErrorCode implements ErrorCode {
 	NOTICE_NOT_FOUND(HttpStatus.NOT_FOUND, "알림을 찾지 못했습니다."),
 	NOT_MY_NOTICE(HttpStatus.BAD_REQUEST, "내 알림이 아닙니다."),
-	ALREADY_DELETED_NOTICE(HttpStatus.BAD_REQUEST, "이미 삭제된 알림입니다")
+	ALREADY_DELETED_NOTICE(HttpStatus.BAD_REQUEST, "이미 삭제된 알림입니다"),
+	NOT_DELETED(HttpStatus.EXPECTATION_FAILED, "삭제에 실패했습니다.")
 	;
 
 	private final HttpStatus httpStatus;
