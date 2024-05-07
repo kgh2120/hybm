7c10fbc5e96c724895208e52c36fee178ab80584
JangHyunsu <wkd940709@naver.com>
2024-05-07 23:04:07 +0900

    refactor: 알림 api 파일 리팩토링 및 주석 추가

--- frontend/src/api/notificationApi.ts
+++ frontend/src/api/notificationApi.ts
@@ -1,25 +1,27 @@
 import instance from "./axios";
 
-export async function getIsNewNotification() {
+// 미확인 알림 여부 조회(빨간점)
+const getIsNewNotification = async () => {
   try {
     const res = await instance.get("/api/notices/hasnew");
-    console.log(res);
-    return res.status;
+    return res.data;
   } catch (e) {
     console.log(e);
   }
 }
 
-export async function deleteAllNotification() {
+// 알텅텅(전부삭제)
+const deleteAllNotification = async () => {
   try {
-    const res = await instance.delete("/api/notices");
+    const res = await instance.delete("/api/notices/all");
     console.log(res);
   } catch (e) {
     console.log(e);
   }
 }
 
-export async function getNotificationList() {
+// 알림 전체 조회(확인한 것도)
+const getNotificationList = async () => {
   try {
     const res = await instance.get("/api/notices", {
       params: {
@@ -34,7 +36,8 @@ export async function getNotificationList() {
   }
 }
 
-export async function deleteNotification(noticeId: number) {
+// 알림 삭제
+const deleteNotification = async (noticeId: number) => {
   try {
     const res = await instance.delete(`/api/notices/${noticeId}`, {
     });
@@ -44,3 +47,5 @@ export async function deleteNotification(noticeId: number) {
     console.log(e);
   }
 }
+
+export { getIsNewNotification, deleteAllNotification, getNotificationList, deleteNotification }
