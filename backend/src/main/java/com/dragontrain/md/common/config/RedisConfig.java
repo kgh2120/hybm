a30f9e0aa29632a78668867aa724939b7b5894ff
김규현 <kgh2120@gmail.com>
2024-05-08 16:24:09 +0900

    chore: 캐시 ttl 수정

--- backend/src/main/java/com/dragontrain/md/common/config/RedisConfig.java
+++ backend/src/main/java/com/dragontrain/md/common/config/RedisConfig.java
@@ -62,7 +62,7 @@ public class RedisConfig {
 		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
 			.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
 			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())) // Value Serializer 변경
-			.entryTtl(Duration.ofMinutes(3L)); // 캐시 수명 30분
+			.entryTtl(Duration.ofMinutes(30L)); // 캐시 수명 30분
 		return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory()).cacheDefaults(redisCacheConfiguration).build();
 	}
 }
