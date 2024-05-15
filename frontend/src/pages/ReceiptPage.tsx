334eb433070fd1dc873a50d1a904092c12484433
wkd940709 <wkd940709@naver.com>
2024-05-15 17:01:41 +0900

    feat: 영수증 등록 api

--- frontend/src/pages/ReceiptPage.tsx
+++ frontend/src/pages/ReceiptPage.tsx
@@ -3,13 +3,24 @@ import Button from "../components/common/Button";
 import Header from "../components/common/Header";
 import styles from "../styles/receiptPage/ReceiptPage.module.css";
 import home from "../assets/images/home.png";
-// import { useInput } from "../hooks/useInput";
-// import { useState } from "react";
 import FoodSection from "../components/common/FoodSection";
 import { useFoodCategoryStore } from "../stores/useFoodStore";
+import useAuthStore from "../stores/useAuthStore";
+import { useMutation } from "@tanstack/react-query";
+import { postReceipt } from "../api/receiptApi";
+import { useEffect } from "react";
 
 function ReceiptPage() {
   const { bigCategoryList } = useFoodCategoryStore();
+  const { imagePath } = useAuthStore();
+
+  const { mutate: mutatePostReceipt, data: namePriceList } = useMutation({
+    mutationFn: postReceipt,
+  });
+
+  useEffect(() => {
+    mutatePostReceipt(imagePath);
+  }, [namePriceList])
   console.log(bigCategoryList);
 
   return (
