8980b75c242e3065d2e21fd0f7b878f3c39b092c
dani <dlekdms7234@naver.com>
2024-05-16 20:09:45 +0900

    style: 자잘바리 css 수정

--- frontend/src/components/storagePage/FoodStateSection.tsx
+++ frontend/src/components/storagePage/FoodStateSection.tsx
@@ -3,17 +3,19 @@ import styles from "../../styles/common/FoodStateSection.module.css"
 interface FoodStateSectionProps {
   sectionTitle: string;
   sectionClass: string;
+  wrapperClass: string;
   children: React.ReactNode;
 }
 
 function FoodStateSection({
   sectionTitle,
   sectionClass,
+  wrapperClass,
   children,
 }: 
 FoodStateSectionProps) {
   return (
-    <div className={styles.wrapper}>
+    <div className={styles[wrapperClass]}>
       <h2>{sectionTitle}</h2>
       <section className={styles[sectionClass]}>{children}</section>
     </div>
