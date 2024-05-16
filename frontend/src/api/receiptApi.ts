073f91fd9e1db612714dc2c5d38959c0855fd782
d2doo <hzxvl@naver.com>
2024-05-16 15:15:56 +0900

    feat: 리액트 제발 그만

--- frontend/src/api/receiptApi.ts
+++ frontend/src/api/receiptApi.ts
@@ -1,14 +1,45 @@
 import instance from "./axios";
 
 // 영수증 OCR 요청
-const postReceipt = async (imagePath: string) => {
-    console.log(imagePath);
+const postReceipt = async (image: File | null) => {
+  if (!image) {
+    console.error("No image file provided");
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append("image", image);
+  console.log(image);
+
   try {
-    const res = await instance.post("/api/foods/getReceiptOCR", imagePath);
+    const res = await instance.post(
+      "/api/foods/getReceiptOCR",
+      formData
+    );
+    alert(`테스트: ${image.name}, ${formData}, ${res}`);
     return res.data;
   } catch (e) {
+    alert(`실패: ${image.name}, ${formData}, ${e}`);
     console.log(e);
   }
 };
 
-export { postReceipt };
+interface FoodDataType {
+  name: string;
+  categoryId: number;
+  price: number;
+  expiredDate: string;
+  location: string;
+}
+
+const postFoodByReceipt = async (foodList: FoodDataType[]) => {
+  try {
+    const res = await instance.post("/api/foods/bill", foodList);
+    return res.data;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+export { postReceipt, postFoodByReceipt };
--- frontend/src/pages/ReceiptPage.tsx
+++ frontend/src/pages/ReceiptPage.tsx
@@ -1,42 +1,78 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../components/common/Button";
 import Header from "../components/common/Header";
 import styles from "../styles/receiptPage/ReceiptPage.module.css";
 import home from "../assets/images/home.png";
-import FoodSection from "../components/common/FoodSection";
+// import FoodSection from "../components/common/FoodSection";
 import { useFoodCategoryStore } from "../stores/useFoodStore";
 import useAuthStore from "../stores/useAuthStore";
 import { useMutation } from "@tanstack/react-query";
-import { postReceipt } from "../api/receiptApi";
-import { useEffect } from "react";
+import { postFoodByReceipt, postReceipt } from "../api/receiptApi";
+
+// interface namePriceType {
+//   name: string;
+//   price: number;
+// }
 
 function ReceiptPage() {
+  const navigate = useNavigate();
   const { bigCategoryList } = useFoodCategoryStore();
-  const { imagePath } = useAuthStore();
+  const { image } = useAuthStore();
 
-  const { mutate: mutatePostReceipt, data: namePriceList } = useMutation({
+  const {
+    mutate: mutatePostReceipt,
+    data: namePriceList,
+    status,
+  } = useMutation({
     mutationFn: postReceipt,
   });
 
+  console.log(namePriceList);
+
+  const { mutate: mutatePostFoodByReceipt } = useMutation({
+    mutationFn: postFoodByReceipt,
+    onSuccess: () => {
+      navigate("/");
+    },
+  });
+
+  const handlePostFoodByReceipt = () => {
+    console.log(mutatePostFoodByReceipt);
+    // mutatePostFoodByReceipt();
+  };
+
   useEffect(() => {
-    mutatePostReceipt(imagePath);
-  }, [namePriceList])
+    if (image) {
+      alert(`Uploading image: ${image.name}`);
+      mutatePostReceipt(image);
+    }
+  }, [image]);
   console.log(bigCategoryList);
+
+  if (status === "pending") {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.white_wrapper}>
-        <Header title="영수증 등록" />
+        <Header title="영수증 등록 테스트" />
         <Link to="/">
           <img className={styles.home_img} src={home} alt="" />
         </Link>
         <section className={styles.food_list_section}>
-          <FoodSection option="receipt"/>
-
-          <FoodSection option="receipt"/>
-          <FoodSection option="receipt"/>
+          {/* {namePriceList.map((namePrice: namePriceType) => {
+            console.log(namePrice)
+            return <FoodSection option="active"/>
+          })} */}
         </section>
-        <Button content="완료" color="red" onClick={() => {}} disabled={false}/>
+        <Button
+          content="완료"
+          color="red"
+          onClick={handlePostFoodByReceipt}
+          disabled={false}
+        />
       </div>
     </div>
   );
