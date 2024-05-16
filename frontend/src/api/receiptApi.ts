033ee75c85fe783c2678aec51f73d885ef072c7a
wkd940709 <wkd940709@naver.com>
2024-05-16 15:09:14 +0900

    feat: 이미지 디코딩

--- frontend/src/api/receiptApi.ts
+++ frontend/src/api/receiptApi.ts
@@ -1,9 +1,31 @@
 import instance from "./axios";
 
 // 영수증 OCR 요청
-const postReceipt = async (image: File | null) => {
+const postReceipt = async (image: string | null) => {
+  const base64ToBlob = (image: string, mimeType: string) => {
+    const byteCharacters = atob(image); // base64 디코딩
+    const byteArrays = [];
+  
+    for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+      const slice = byteCharacters.slice(offset, offset + 512);
+  
+      const byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+  
+      const byteArray = new Uint8Array(byteNumbers);
+      byteArrays.push(byteArray);
+    }
+  
+    return new Blob(byteArrays, { type: mimeType }); // Blob 객체 생성
+  };
+
+  const blob = base64ToBlob(image!, 'image/jpeg');
+  const file = new File([blob], 'image.jpg', { type: 'image/jpeg' });
+
   const formData = new FormData();
-  formData.append("image", image!);
+  formData.append("image", file);
   console.log(image);
   try {
     const res = await instance.post(
