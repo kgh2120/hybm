30b766f97a436aaaf10adfb0cb0966740571718e
d2doo <hzxvl@naver.com>
2024-05-17 17:24:41 +0900

    chore: 파 4트

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,9 +1,5 @@
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js"
-);
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js"
-);
+importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyA36ekKWZOdOkcoHgoZLnG28YA9LFJJsN8",
@@ -12,25 +8,19 @@ firebase.initializeApp({
   storageBucket: "haveyoubeenmart.appspot.com",
   messagingSenderId: "26693461212",
   appId: "1:26693461212:web:59e1b86728817c4e760019",
-  measurementId: "G-RJ06HJSH52",
+  measurementId: "G-RJ06HJSH52"
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function (payload) {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    "여기서부터 페이로드다ㅏㅏㅏㅏㅏ", payload
-  );
+messaging.onBackgroundMessage(function(payload) {
+  console.log("[firebase-messaging-sw.js] Received background message ", payload);
 
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/assets/icon/logo.png',
+    icon: '/assets/icon/logo.png'
   };
 
-  self.registration.showNotification(
-    notificationTitle,
-    notificationOptions
-  );
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
