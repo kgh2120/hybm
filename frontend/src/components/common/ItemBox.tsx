c59d6797c50859a486b3ee198a2045dc7ee8a6b8
dani <dlekdms7234@naver.com>
2024-05-18 19:59:02 +0900

    fix: itemBox 수정

--- frontend/src/components/common/ItemBox.tsx
+++ frontend/src/components/common/ItemBox.tsx
@@ -1,5 +1,6 @@
 import styles from "../../styles/common/ItemBox.module.css";
-import check from "../../assets/images/check.png"
+import check from "../../assets/images/check.png";
+import { useLocation } from "react-router-dom";
 
 interface ItemBoxProps {
   name: string;
@@ -15,6 +16,11 @@ function ItemBox({
   imgSrc,
   onClick,
 }: ItemBoxProps) {
+  const location = useLocation();
+  const nameLength = location.pathname === "/report" ? 3 : 5
+  const fomattedName =
+    name.length > nameLength ? `${name.slice(0, nameLength)}..` : name;
+
   return (
     <article className={styles.wrapper} onClick={onClick}>
       {option === "inactive" ? (
@@ -25,8 +31,16 @@ function ItemBox({
       ) : option === "clicked" ? (
         <div className={styles.clicked_img_box}>
           <div className={styles.img_solid_white_box}></div>
-          <img className={styles.food_img} src={imgSrc} alt="상품아이콘" />
-          <img className={styles.check_img} src={check} alt="체크아이콘" />
+          <img
+            className={styles.food_img}
+            src={imgSrc}
+            alt="상품아이콘"
+          />
+          <img
+            className={styles.check_img}
+            src={check}
+            alt="체크아이콘"
+          />
         </div>
       ) : option === "report" ? (
         <div className={styles.report_img_box}>
@@ -40,7 +54,7 @@ function ItemBox({
         </div>
       )}
       <div className={styles.text_box}>
-        <span className={styles.item_name}>{name}</span>
+        <span className={styles.item_name}>{fomattedName}</span>
         <span className={styles.item_content}>{content}</span>
       </div>
     </article>
